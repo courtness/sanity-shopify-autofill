da1f6d94f1f2bfc1a59b73eccf78c188b5edfdc0
Daniel Courtness <danielcourtness@gmail.com>
2020-12-27 12:30:35 +0900

    Sample from template

---
+++ index.js
@@ -0,0 +1,40 @@
+import fs from "fs";
+
+import shopify from "./data/products.json";
+
+const sanityProducts = [];
+const excludedHandles = [
+  `dyson-supersonic-hair-dryer-black-nickel`,
+  `eleven-give-me-clean-hair-dry-shampoo-130g`,
+  `dyson-supersonic-hair-dryer-fuchsia-iron`
+];
+
+shopify.products.forEach((product, productIndex) => {
+  if (excludedHandles.includes(product.handle)) {
+    return;
+  }
+
+  //
+
+  sanityProducts.push({
+    _id: product.id.toString(),
+    _type: `product`,
+    title: product.title,
+    handle: product.handle,
+    description: product.body_html.replace(/<\/?[^>]+(>|$)/g, ``),
+    image: {
+      _type: `altImage`,
+      _sanityAsset: `image@${product.image.src}`,
+      altText: product.title
+    }
+  });
+});
+
+const dump = sanityProducts.map(JSON.stringify).join(`\n`);
+
+fs.writeFile(`./export/dump.json`, dump, (err) => {
+  if (err) {
+    // eslint-disable-next-line no-console
+    console.error(err);
+  }
+});
